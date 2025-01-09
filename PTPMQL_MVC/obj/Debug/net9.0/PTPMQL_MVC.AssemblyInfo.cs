1c55838b468885e90aed81b2bc9bf9ed989ba1f4
manhtu1509 <haizzt3n@gmail.com>
2025-01-09 23:51:03 +0700

    create_project

--- PTPMQL_MVC/obj/Debug/net9.0/PTPMQL_MVC.AssemblyInfo.cs
+++ PTPMQL_MVC/obj/Debug/net9.0/PTPMQL_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c651ed5568e7155e8900c849b9a5c9bc2ca76485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69ead8e0b6e2e1df6f8c11acc51f3ea2164f3eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
