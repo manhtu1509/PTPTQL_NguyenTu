882627d05de5409868a7f93f2c2bbe3f93b93564
manhtu1509 <haizzt3n@gmail.com>
2025-01-15 15:48:40 +0700

    PTPTMQL_MANHTU

--- PTPMQL_MVC/obj/Debug/net9.0/PTPMQL_MVC.AssemblyInfo.cs
+++ PTPMQL_MVC/obj/Debug/net9.0/PTPMQL_MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c55838b468885e90aed81b2bc9bf9ed989ba1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4e7fbf7bd12e1f7521fb8efaa91fe8efdb2a90")]
 [assembly: System.Reflection.AssemblyProductAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PTPMQL_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
