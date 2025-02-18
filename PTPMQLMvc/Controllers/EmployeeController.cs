046942eac38cfece81d781b45cb74f0dc2402d98
manhtu1509 <haizzt3n@gmail.com>
2025-02-18 13:17:17 +0700

    Baitap1202

---
+++ PTPMQLMvc/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PTPMQLMvc.Models;
+
+namespace PTPMQLMvc.Controllers;
+
+public class EmployeeController : Controller
+{
+    private readonly ILogger<EmployeeController> _logger;
+
+    public EmployeeController(ILogger<EmployeeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult EmployeeIndex()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
