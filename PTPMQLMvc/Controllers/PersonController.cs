ef2cb6a1593bd62689a4281214521a3b8a502c49
manhtu1509 <haizzt3n@gmail.com>
2025-03-01 21:06:22 +0700

    Baitap2509

--- PTPMQLMvc/Controllers/PersonController.cs
+++ PTPMQLMvc/Controllers/PersonController.cs
@@ -1,21 +1,87 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PTPMQLMvc.Models;
+using PTPMQLMvc.Data;
 
 namespace PTPMQLMvc.Controllers;
 
 public class PersonController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    public PersonController(ApplicationDbContext context){
+        _context = context;
+    }
+    public async Task<IActionResult> Index(){
+        var model = await _context.Person.ToListAsync();
+        return View(model);
+    }
+    public IActionResult Create()
+    {
+        return View();
+    }
     public IActionResult Index(){
         return View();
     }
     [HttpPost]
-    public  IActionResult Index(Person ps)
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult>create([Bind("Person,Fullname,Address")] Person person)
     {
-        string strOutput = "Xin chao" + ps.PersonId + "-" + ps.FullName + "-" + ps.Address;
-        ViewBag.infoPerson = strOutput;
-        return View();
+        if (ModelState.IsValid)
+        {
+            _context.Add(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(index));
+
+        }
+        return View(person);
+
     }
+    public async Task<IActionResult>Edit(string id)
+    {
+        if (id == null || _context.Person ==  null)
+        {
+            return NotFound();
+        }
+        return View(person);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult>Edit(string id, [Bind("PersonId, Fullname, Address")] Person person)
+    {
+        if (id != person.PersonId)
+        {
+            return NotFound();
+        }
+        if (ModelState.IsValid)
+        {
+            try{
+                _context.Update(person);
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(person.PersonId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(person);//đã đến slide 52
+    }
+    public async Task<IActionResult>Delete(string id);
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(string id)
+    private bool PersonExists(string id);
+    [HttpPost]
+    
     private readonly ILogger<PersonController> _logger;
 
     public PersonController(ILogger<PersonController> logger)
---
+++ PTPMQLMvc/Data/ApplicationDbcontext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PTPMQLMvc.Models; 
+
+namespace PTPMQLMvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; } 
+    }
+}
